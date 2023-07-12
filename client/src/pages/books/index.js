7d7531573391f14d2ec6eb3231b5cad317a7d3f2
Nirmal Savinda <nirmalsavinda29@gmail.com>
2023-07-12 19:43:13 +0530

    add dialog

--- client/src/pages/books/index.js
+++ client/src/pages/books/index.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import * as React from 'react'
 
 import Dashboard from './dashboard'
 import Add from './add'
 import Delete from './delete'
 import Update from './update'
-import { Box, Typography,Button } from '@mui/material'
+import { Box, Typography,Button, Dialog, DialogContent, DialogContentText } from '@mui/material'
 
 export default function Books() {
+    const [open,setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
   return (
     <div>
         <Box sx={{ml:14}}>
 
         <Typography variant='h3'>Books</Typography> 
-        <Button>Add Book</Button>
+        <Button variant='outlined' onClick={handleClickOpen} >Add Book</Button>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogContent>
+                <DialogContentText>
+                    Hey
+                </DialogContentText>
+            </DialogContent>
+        </Dialog>
         <Dashboard />
 
         </Box>
