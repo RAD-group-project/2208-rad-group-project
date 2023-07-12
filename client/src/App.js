947063b1e2e1812fcc01c1d6de1fce81b28e2de2
Nirmal Savinda <nirmalsavinda29@gmail.com>
2023-07-12 06:31:33 +0530

    added sidebar with mui,routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Sidebar >
+          fdfdfs
+        </Sidebar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +25,19 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Router> */}
+       <Routes>
+            <Route exact path="/" element={"Home"}>
+              {/* Home */}
+            </Route>
+            <Route exact path="/login" element={"Login"}>
+            
+            </Route>
+            <Route exact path="/books" element={"Books"}>
+            
+            </Route>
+            </Routes>
+        {/* </Router> */}
       </header>
     </div>
   );
---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Drawer from '@mui/material/Drawer'
+import { makeStyles } from '@mui/styles'
+import { Typography, List, ListItem,ListItemButton} from '@mui/material'
+
+import {Link} from 'react-router-dom'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles({
+    page: {
+        background: '#f9f9f9',
+        width:'100%'
+ 
+    },
+    drawer: {
+        width: drawerWidth
+    }
+})
+
+export default function Sidebar() {
+    const classes = useStyles()
+  return (
+    <div>
+        <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        anchor='left'
+        >
+            <div>
+<Typography variant='h5'>
+    <Link to="/">
+    <ListItemButton>
+    Home
+    </ListItemButton>
+    </Link>
+
+</Typography>
+<Typography variant='h5'>
+<Link to="books">
+
+
+    <ListItemButton>
+    Books
+    </ListItemButton>
+    </Link>
+
+</Typography>
+            </div>
+        </Drawer>
+    </div>
+  )
+}
