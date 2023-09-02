a6e2170ce03b210dd252e33c1db8328f5330b866
Siyara Wijewardane <2021cs221@stu.ucsc.cmb.ac.lk>
2023-09-02 10:38:52 +0530

    Added authour models

---
+++ server/models/authour.js
@@ -0,0 +1,43 @@
+// Importing modules
+const mongoose = require("mongoose");
+
+// Creating book schema
+const AuthorSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: false,
+  },
+  nationality: {
+    type: String,
+    required: false,
+  },
+  startDateOfPublishing: {
+    type: Date,
+    required: false,
+  },
+
+  genres: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Genre",
+    },
+  ],
+
+  booksWritten: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+  ],
+});
+
+// Creating the Author model
+module.exports = mongoose.model("Authour", AuthourSchema);
