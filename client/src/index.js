17bbe5d05077a74e408abc98c7fd8d29272ecc7c
nsavinda <nirmalsavinda29@gmail.com>
2023-09-05 08:50:55 +0530

    fix:makestyle inject issue

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from "./hooks/useAuth";
+import { StyledEngineProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <BrowserRouter>
     <AuthProvider>
+      <StyledEngineProvider injectFirst>
     <App />
+    </StyledEngineProvider>
     </AuthProvider>
     </BrowserRouter>
   // </React.StrictMode>
