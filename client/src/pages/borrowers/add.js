11854cdbfbe6cc569a548bb79ce2b489e06b6523
vishwanthieherath <vishwanthieherath@gmail.com>
2023-09-10 06:55:28 +0530

    Add borrowers to the pages

---
+++ client/src/pages/borrowers/add.js
@@ -0,0 +1,203 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+
+import { backendUrl } from "../../data";
+
+const validationSchema = yup.object({
+    userID: yup.string("Enter member ID number").required("Member ID is required"),
+    firstName: yup.string("Enter first name").required("First name is required"),
+    lastName: yup.string("Enter last name").required("Last name is required"),
+    ISBN: yup.string('Enter ISBN').required('ISBN is required'),
+    title: yup.string("Enter book title").required("Title is required"),
+    author: yup.string("Enter the author").required("Title is required"),
+    checkoutDate: yup.date("Enter checkout date").required("Checkout date is required"),
+    dueDate: yup.date("Enter due date").required("Due date is required"),
+    // returnDate: yup.string("Enter returned date").required("Return date is required"),
+    email: yup.string("Enter email address").email("Email is required"),
+    phone: yup.number("Enter contact number").typeError('Enter a valid number'),
+});
+
+// const apiUrl = backendUrl + 'book/add'
+
+const addBorrowerApi = async (borrowerData) => {
+  try {
+    const response = await axios.post(`${backendUrl}borrower/add`, borrowerData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const Add = ({ handleClose, updateTrigger }) => {
+  const [borrower, setBorrower] = useState({
+    userID: "",
+    firstName: "",
+    lastName: "",
+    ISBN: "",
+    title: "",
+    author: "",
+    checkoutDate: "",
+    dueDate: "",
+    email: "",
+    phone: "",
+  });
+
+  const formik = useFormik({
+    initialValues: borrower,
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await addBorrowerApi(values);
+        alert("Borrower added successfully");
+
+        handleClose();
+        updateTrigger();
+      } catch (error) {
+        if (error.response && error.response.status === 403) {
+          alert("Borrower already exists");
+        } else {
+          console.log(error);
+          alert("Something went wrong, please try again");
+        }
+      }
+    },
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="userID"
+          name="userID"
+          label="User ID"
+          value={formik.values.userID}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.userID && Boolean(formik.errors.userID)}
+          helperText={formik.touched.userID && formik.errors.userID}
+        />
+
+        <TextField
+          fullWidth
+          id="firstName"
+          name="firstName"
+          label="First Name"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+          helperText={formik.touched.firstName && formik.errors.firstName}
+        />
+
+        <TextField
+          fullWidth
+          id="lastName"
+          name="lastName"
+          label="Last Name"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.lastName && formik.errors.lastName}
+        />
+
+        <TextField
+          fullWidth
+          id="ISBN"
+          name="ISBN"
+          label="ISBN"
+          value={formik.values.ISBN}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.ISBN && Boolean(formik.errors.ISBN)}
+          helperText={formik.touched.ISBN && formik.errors.ISBN}
+        />
+
+        <TextField
+          fullWidth
+          id="title"
+          name="title"
+          label="Title"
+          value={formik.values.title}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.title && Boolean(formik.errors.title)}
+          helperText={formik.touched.title && formik.errors.title}
+        />
+
+        <TextField
+          fullWidth
+          id="author"
+          name="author"
+          label="Author"
+          value={formik.values.author}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.author && Boolean(formik.errors.author)}
+          helperText={formik.touched.author && formik.errors.author}
+        />
+
+        <TextField
+          fullWidth
+          id="checkoutDate"
+          name="checkoutDate"
+          label="Checkout Date"
+          value={formik.values.checkoutDate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.checkoutDate && Boolean(formik.errors.checkoutDate)
+          }
+          helperText={formik.touched.checkoutDate && formik.errors.checkoutDate}
+        />
+
+        <TextField
+          fullWidth
+          id="dueDate"
+          name="dueDate"
+          label="Due Date"
+          value={formik.values.dueDate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.dueDate && Boolean(formik.errors.dueDate)}
+          helperText={formik.touched.dueDate && formik.errors.dueDate}
+        />
+
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+
+        <TextField
+          fullWidth
+          id="phone"
+          name="phone"
+          label="Phone"
+          type="number"
+          value={formik.values.phone}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.phone && Boolean(formik.errors.phone)}
+          helperText={formik.touched.phone && formik.errors.phone}
+        />
+
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
---
+++ client/src/pages/borrowers/dashboard.js
@@ -0,0 +1,241 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import { MaterialReactTable } from 'material-react-table';
+
+import { Box, Button, Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions, ListItemIcon, MenuItem, Typography, Tooltip, IconButton } from '@mui/material';
+import { AccountCircle, Send, Delete, Edit } from '@mui/icons-material';
+import Update from './update';
+
+import { backendUrl } from '../../data';
+import axios from 'axios';
+
+const Dashboard = ({trigger}) => {
+
+  const [borrowers, setBorrowers] = useState([]);
+  const [isTableLoading, setIsTableLoading] = useState(true);
+
+  useEffect(() => {
+    getAllBooks();
+  }, [trigger]);
+
+  const getAllBorrowers = () => {
+    console.log(backendUrl + 'borrower/getAll')
+    axios.get(backendUrl + 'borrower/getAll')
+      .then((response) => {
+        // handle success
+        console.log(response.data);
+        const members = response.data.borrowersList;
+        setBorrower(members);
+        setIsTableLoading(false);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  };
+
+
+  const deleteBorrower = (borrowerId) => {
+    axios.delete(`${backendUrl}book/delete/${borrowerId}`)
+      .then((response) => {
+        handleClose();
+        console.log(response.data);
+        getAllBorrowers();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+  const [openUpdate,setOpenUpdate] = useState(false);
+  const [openDelete,setOpenDelete] = useState(false);
+  const [selectedBorrower, setSelectedBorrower] = useState(null); // Add selectedBook state
+  const [selectedBorrowerId, setSelectedBorrowerId] = useState(null);
+
+
+
+  const handleClickUpdate = (borrower) => {
+    setSelectedBorrower(borrower);
+    setOpenUpdate(true);
+  }
+
+  const handleClickDelete = (borrower) => {
+    console.log(borrower["row"]["original"]["_id"]);
+    setSelectedBorrowerId(borrower["row"]["original"]["_id"]);
+    setOpenDelete(true);
+  }
+  const handleClose = () => {
+    setOpenUpdate(false);
+    setOpenDelete(false);
+  }
+
+  
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "userID",
+        header: "User ID",
+        size: 150,
+      },
+      {
+        accessorKey: "firstName",
+        header: "First Name",
+        size: 150,
+      },
+      {
+        accessorKey: "lastName",
+        header: "Last Name",
+        size: 150,
+      },
+      {
+        accessorKey: "ISBN",
+        header: "ISBN",
+        size: 150,
+      },
+      {
+        accessorKey: "title",
+        header: "Title",
+        size: 150,
+      },
+      {
+        accessorKey: "author",
+        header: "Author",
+        size: 150,
+      },
+      {
+        accessorKey: "checkoutDate",
+        header: "Checkout Date",
+        size: 150,
+      },
+      {
+        accessorKey: "dueDate",
+        header: "Due Date",
+        size: 150,
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+        size: 150,
+      },
+      {
+        accessorKey: "phone",
+        header: "Phone Number",
+        size: 150,
+      },
+    ],
+    []
+  );
+
+  
+
+
+  return (
+  <div> <MaterialReactTable 
+  columns={columns}
+  data={borrowers}
+  enableRowActions
+  renderRowActions={( rowData) => (
+    <Box sx={{ display: 'flex', gap: '1rem' }}>
+      <Tooltip arrow placement="left" title="Edit">
+        <IconButton onClick={() => handleClickUpdate(rowData)}>
+          <Edit />
+        </IconButton>
+      </Tooltip>
+      <Tooltip arrow placement="right" title="Delete">
+        <IconButton color="error" onClick={() => handleClickDelete(rowData)}>
+          <Delete />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  )}
+  renderRowActionMenuItems={({ closeMenu, rowData }) => [
+    
+    <MenuItem
+      key={0}
+      onClick={() => {
+        // View profile logic...
+        handleClickUpdate(rowData);
+        closeMenu()
+      }}
+      sx={{ m: 0 }}
+    >
+      <ListItemIcon>
+        <AccountCircle />
+      </ListItemIcon>
+      Update
+    </MenuItem>,
+    <MenuItem
+      key={1}
+      onClick={() => {
+        // Send email logic...
+        handleClickDelete()
+        closeMenu();
+      }}
+      sx={{ m: 0 }}
+    >
+      <ListItemIcon>
+        <Send />
+      </ListItemIcon>
+      Delete
+    </MenuItem>,
+  ]}
+//   renderDetailPanel={({ row }) => (
+//     <Box
+//       sx={{
+//         display: 'flex',
+//         justifyContent: 'space-around',
+//         alignItems: 'center',
+//       }}
+//     >
+//       <img
+//         alt={row.original.title}
+//         height={200}
+//         src={`https://covers.openlibrary.org/b/isbn/${row.original.ISBN}-M.jpg`}
+//         loading="lazy"
+//         // style={{ borderRadius: '50%' }}
+//       />
+//       {console.log(row)}
+//       <Box sx={{ textAlign: 'center' }}>
+//         <Typography variant="h4">{row.original.title}</Typography>
+//         <Typography variant="h5">
+//           {row.original.author}
+//         </Typography>
+//       </Box>
+//     </Box>
+//   )}
+  
+  />
+
+<Dialog open={openUpdate} onClose={handleClose}>
+            <DialogContent>
+                {/* <DialogContentText> */}
+                  <Update book={selectedBorrower} handleClose={handleClose} getAllBorrowers={getAllBorrowers}/>
+                  {/* </DialogContentText> */}
+            </DialogContent>
+        </Dialog>
+
+      <Dialog open={openDelete} onClose={handleClose}>
+      <DialogTitle id="alert-dialog-title">
+          {" Delete This Borrower?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Delete This Borrower?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => deleteBorrower(selectedBorrowerId)}>Yes</Button>
+          <Button onClick={handleClose} autoFocus>
+            No
+          </Button>
+        </DialogActions>
+        </Dialog>
+  </div>
+  
+  
+  
+  );
+};
+
+export default Dashboard;
