57194c6da48409e366999815e2a027080760df31
vishwanthieherath <vishwanthieherath@gmail.com>
2023-09-10 08:02:35 +0530

    Add borrower to models

---
+++ server/models/borrower.js
@@ -0,0 +1,48 @@
+// Importing modules
+const mongoose = require("mongoose");
+
+// Creating book schema
+const BorrowerSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  ISBN: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  checkoutDate: {
+    type: Date,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Borrower", BorrowerSchema);
