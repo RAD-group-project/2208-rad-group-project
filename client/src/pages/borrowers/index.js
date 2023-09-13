eb55bebcd2c1e43d48fa9ba10b3de5bd9f22d9f3
vishwanthieherath <vishwanthieherath@gmail.com>
2023-09-13 21:09:43 +0530

    Add styles

--- client/src/pages/borrowers/index.js
+++ client/src/pages/borrowers/index.js
@@ -25,21 +25,29 @@ export default function Borrowers() {
     const useStyles = makeStyles({
       booksContainer: {
         background: `
-        linear-gradient(to bottom, #003f5c, #2f4b7c),
+        linear-gradient(to bottom, #272643, #2f2e52),
         radial-gradient(circle, rgba(0, 0, 0, 0.2) 10%, rgba(0, 0, 0, 0) 70%)`,
         backgroundBlendMode: "multiply, normal",
         minHeight: "100vh", // Adjust as needed
         padding: "20px",
         color: "#ffffff",
-        textAlign: "center",
+        textAlign: "left",
+      },
+      header: {
+        display: "flex",
+        justifyContent: "space-between",
+        marginBottom: "10px"
       },
       sectionTitle: {
-        fontSize: "32px",
+        fontSize: "40px",
         marginBottom: "20px",
+        marginTop: "20px",
+        paddingLeft: "25px"
       },
       addButton: {
         color: "#ffffff",
         borderColor: "#ffffff",
+        marginRight: "20px"
       },
     });
 
@@ -49,19 +57,20 @@ export default function Borrowers() {
   return (
     <div className={classes.booksContainer}>
       <Box sx={{ ml: 28, my: 5 }}>
-        <Typography variant="h3" className={classes.sectionTitle}>
-          Borrowers
-        </Typography>
-        <Box sx={{ mx: 2, my: 3 }}>
-          <Button
-            variant="outlined"
-            onClick={handleClickOpen}
-            className={classes.addButton}
-          >
-            Add Borrowers
-          </Button>
+        <Box className={classes.header}>
+          <Typography variant="h3" className={classes.sectionTitle}>
+            Borrowers
+          </Typography>
+          <Box sx={{ mx: 2, my: 3 }}>
+            <Button
+              variant="outlined"
+              onClick={handleClickOpen}
+              className={classes.addButton}
+            >
+              Add Borrower
+            </Button>
+          </Box>
         </Box>
-
         <Dialog open={open} onClose={handleClose}>
           <DialogContent>
             <Add handleClose={handleClose} updateTrigger={updateTrigger} />
@@ -74,9 +83,10 @@ export default function Borrowers() {
             boxShadow:
               "0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23)",
             borderRadius: "8px", // Rounded corners for a card-like appearance
-            backgroundColor: "#ffffff", // Card background color
+            backgroundColor: "#cccccc", // Card background color
             padding: "16px", // Padding within the card
             opacity: 0.9,
+            margin: "20px"
           }}
         >
           <Dashboard trigger={trigger} />
