179b76a7e15b86222b4a4da59a4928de4c83bebf
nsavinda <nirmalsavinda29@gmail.com>
2023-09-15 07:25:23 +0530

    style:dashboard text align

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -46,6 +46,7 @@ export default function Dashboard() {
       fontWeight: 'bold',
       textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
       color: '#fff',
+      textAlign:'center'
     },
   });
 
